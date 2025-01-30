d7d60c3c5dd1afc431bb0b888dfb5daed0465d5e
jithin <jithinjzx@gmail.com>
2025-01-30 13:37:56 +0530

    vercel

--- vercel_wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fi.settings')
-
-# Get the WSGI application
-app = get_wsgi_application()  # Vercel expects `app`
